b30683b3a5b306ec667ddadd26968025284552c4
Jay G <playerjayho@gmail.com>
2017-11-17 14:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
   
 }
