58abe2e6e117c03164b3aa88f87cddb320cbf6c9
Jay G <playerjayho@gmail.com>
2017-11-17 14:49:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var recipes = { eggs: "one", milk: "two" }
 
-function updateObjectWithKeyAndValue(object, key, value) {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+      object.key = value;
+      return object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
